e637d97f39641b103eb527b94d78dd3fba2ceb8f
SARB <mrsinghbusiness05@gmail.com>
2023-12-06 12:24:34 +0530

    frontend folder re-strucutred

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
 
   return (
     <>
-     Hello in Frontend
-     </>
+     HelloFrontend 
+    </>
   )
 }
 
