297bfdc937f5a151141eeb6cacf8f29ebe3e6142
SARB <mrsinghbusiness05@gmail.com>
2024-01-03 14:04:16 +0530

    backend url - adde in viteconfig

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server : {
     proxy : {
       '/api' : {
-        target : 'http://localhost:4000',
+        target : 'https://one0xcourses-backend.onrender.com',
         changeOrigin :true
        }
     }
