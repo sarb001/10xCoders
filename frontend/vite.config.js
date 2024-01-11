160b46a38826f68279921d6a2f796aca1afbcbd1
SARB <mrsinghbusiness05@gmail.com>
2024-01-12 04:26:38 +0530

    remove proxy/adding url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,12 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server : {
-    proxy : {
-      '/api' : {
-        target : 'https://one0xcourses-backend.onrender.com/api',
-        changeOrigin :true,
-        rewrite : (path) => path.replace(/^\/api/,''),
-       }
-    }
+    // proxy : {
+    //   '/api' : {
+    //     // target : 'https://one0xcourses-backend.onrender.com/api',
+    //     // changeOrigin :true,
+    //     // rewrite : (path) => path.replace(/^\/api/,''),
+    //    }
+    // }
   }
 })
