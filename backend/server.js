057fa7019b49b2b87108c16d0f1de76cd4746b2b
SARB <mrsinghbusiness05@gmail.com>
2024-01-14 16:49:06 +0530

    server file restructured

--- backend/server.js
+++ backend/server.js
@@ -18,15 +18,16 @@ dotenv.config({path:'../backend/config.env'});
 connectDb();
 
 // middlewars
+
+app.use(express.json({limit : '50mb'}));
+app.use(express.urlencoded({extended : false }));
+app.use(cookieParser());
+
 app.use(cors({
       origin : process.env.FRONTEND_URL_ORIGIN,
       credentials: true,
       methods: ["GET", "POST", "PUT", "DELETE"],
 }))
-
-app.use(express.json({limit : '50mb'}));
-app.use(express.urlencoded({extended :true}));
-app.use(cookieParser());
 
 
 const  PORT = process.env.PORT;
